6601f1caf6d6743374c5bd2b59c5b330b1274e3b
Marcello Domingues Pereira <marcellinho.domingues@live.com>
2023-02-12 20:39:04 -0300

    Implementando Controllers Person e Book

---
+++ RestWithASPNETUdemy/04_RestWithASPNETUdemy_BancoDados/RestWithASPNETUdemy/RestWithASPNETUdemy/db/migrations/V3__Create_Table_Book.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS `books` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `price` decimal(10, 2) NOT NULL,
+  `launch_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+)
