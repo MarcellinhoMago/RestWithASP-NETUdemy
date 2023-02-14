07ccf552b76840a565a7cb5d08240b01a7723312
Marcello Domingues Pereira <marcellinho.domingues@live.com>
2023-02-13 22:01:57 -0300

    Implementando o Padrão de Projetos VO

---
+++ RestWithASPNETUdemy/06_RestWithASPNETUdemy_ParseVO/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Rio de Janeiro - Brasil', 'Marcello', 'Male', 'Pereira'),
+	(2, 'São Paulo - Brasil', 'Jennifer', 'Female', 'Aline Marques da Silva'),
+	(3, 'Rio de Janeiro - Brasil', 'Lucas', 'Male', 'Alves Pereira'),
+	(4, 'Niterói - Brasil', 'Lorena', 'Female', 'Pereira');
